56648bc21ce1840b2ee4ca360676a4bc1768a8c4
nguyenvl179 <lephucnguyen17052000@gmail.com>
2023-01-28 23:46:23 +0700

    Add day 3

---
+++ 10-days-js/day-3/try-catch-finally.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    try{
+        const arrString = s.split('');
+        arrString.reverse();
+        const strArr = arrString.join('');
+        console.log(strArr);
+    }catch (ex) {
+        console.log(ex.message)
+        console.log(s)
+    }
+}
+
+
+function main() {
+    const s = eval(readLine());
+    
+    reverseString(s);
+}
